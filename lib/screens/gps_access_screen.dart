d210095127e36539ce20323b5dfc8f1345e6c5ea
laubrener <79546818+laubrener@users.noreply.github.com>
2023-07-25 15:52:09 -0300

    first commit

---
+++ lib/screens/gps_access_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GPSAccessScreen extends StatelessWidget {
+  const GPSAccessScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('GPSAccessScreen'),
+      ),
+    );
+  }
+}
---
+++ lib/screens/map_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MapsScreen extends StatelessWidget {
+  const MapsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('MapsScreen'),
+      ),
+    );
+  }
+}
